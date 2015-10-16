a2170be33370619073e53f695a91e1896a8b0edf
Johan Alamo <johan.alamo@gmail.com>
2015-10-15 22:07:46 -0430

    Eliminación de módulos

--- modulos/instituto/vista/json/Agregar.php
+++
@@ -1,9 +0,0 @@
-<?php		
-$ins['estatus']=1;
-$ins['mensaje']=Vista::obtenerDato('mensaje');
-
-
-	$cad=json_encode($ins);
-	echo $cad;
-?>
-		
--- modulos/instituto/vista/json/Listar.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$instituto['institutos'] = Vista::obtenerDato('institutos');
-	$instituto['estatus'] = 1;
-	$instituto['mensaje']= Vista::obtenerDato('mensaje');
-	$cad = json_encode($instituto);
-	echo $cad;
-
-?>
